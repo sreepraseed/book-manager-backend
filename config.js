95528d1ffcfdbd4be4ad060c5b6877d43989cb80
Sreepraseed S <sreepraseed18@gmail.com>
2024-02-22 17:40:46 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://sreepraseed18:sree@cluster0.08xj2ae.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorialgit
